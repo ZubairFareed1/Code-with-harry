4286f6ec8d04fa5257ad2c8b76c8ff7edcc98c55
zubair ahmed <zubairahmedjamali908@gmail.com>
2023-12-29 17:50:06 +0500

    Exercise 2 to add color templete but i add a color picker to change the color of navbar but it also change text to black or white according to the dark or light mode

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,43 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { useState } from "react";
 export default function Navbar(props) {
+  const [isDark, setIsDark] = useState(true);
+  const [selectedColor, setSelectedColor] = useState("#003583");
+  let colorModeHandler = () => {
+    return isDark ? "white" : "black";
+  };
+  let colorHandler = () => {
+    const colorPicker = document.getElementById("colorPicker");
+    setSelectedColor(colorPicker.value);
+    console.log(selectedColor);
+    setIsDark(isColorDark(selectedColor));
+    document.body.style.color = isDark ? "#ffffff" : "#000000";
+    function isColorDark(color) {
+      const rgb = parseInt(color.slice(1), 16);
+      const r = (rgb >> 16) & 0xff;
+      const g = (rgb >> 8) & 0xff;
+      const b = (rgb >> 0) & 0xff;
+      const luminance = 0.2126 * r + 0.7152 * g + 0.0722 * b;
+      return luminance < 128;
+    }
+    return selectedColor;
+  };
   return (
     <nav
-      className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}
-      
+      className={`navbar navbar-expand-lg  `}
+      style={{ backgroundColor: `${selectedColor}` }}
     >
       <div className="container">
-        <a className="navbar-brand" href="/">
+        <a
+          className="navbar-brand"
+          href="/"
+          style={{
+            color: `${colorModeHandler()}`,
+            fontSize: "30px",
+            fontWeight: "900",
+          }}
+        >
           Portfolio
         </a>
         <button
@@ -24,12 +54,21 @@ export default function Navbar(props) {
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="/">
+              <a
+                className="nav-link active"
+                aria-current="page"
+                href="/"
+                style={{ color: `${colorModeHandler()}` }}
+              >
                 Home
               </a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="/">
+              <a
+                className="nav-link"
+                href="/"
+                style={{ color: `${colorModeHandler()}` }}
+              >
                 Link
               </a>
             </li>
@@ -40,6 +79,7 @@ export default function Navbar(props) {
                 role="button"
                 data-bs-toggle="dropdown"
                 aria-expanded="false"
+                style={{ color: `${colorModeHandler()}` }}
               >
                 Dropdown
               </a>
@@ -65,7 +105,26 @@ export default function Navbar(props) {
               </ul>
             </li>
           </ul>
-          <div className={`form-check form-switch text-${props.mode==='dark'?'light':'dark'} m-4`}>
+          <label
+            htmlFor="exampleColorInput"
+            className="form-label"
+            style={{ color: `${colorModeHandler()}` }}
+          >
+            Color picker
+          </label>
+          <input
+            type="color"
+            className="form-control form-control-color"
+            id="colorPicker"
+            value={`${selectedColor}`}
+            title="Choose your color"
+            onChange={colorHandler}
+          />
+          <div
+            className={`form-check form-switch text-${
+              props.mode === "dark" ? "light" : "dark"
+            } m-4`}
+          >
             <input
               className="form-check-input"
               type="checkbox"
@@ -73,7 +132,11 @@ export default function Navbar(props) {
               id="flexSwitchCheckDefault"
               onClick={props.toggleMode}
             />
-            <label className="form-check-label" htmlFor="flexSwitchCheckDefault">
+            <label
+              className="form-check-label"
+              htmlFor="flexSwitchCheckDefault"
+              style={{ color: `${colorModeHandler()}` }}
+            >
               Toggle Mode
             </label>
           </div>
@@ -84,7 +147,13 @@ export default function Navbar(props) {
               placeholder="Search"
               aria-label="Search"
             />
-            <button className={`btn ${props.mode==='light'?'btn-outline-primary':'btn-primary'}`} type="submit">
+            <button
+              className={`btn ${
+                props.mode === "light" ? "btn-outline-primary" : "btn-primary"
+              }`}
+              type="submit"
+              style={{ color: `${colorModeHandler()}` }}
+            >
               Search
             </button>
           </form>
